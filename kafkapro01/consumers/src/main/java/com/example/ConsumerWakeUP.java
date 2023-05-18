a77c9a223242799ee3de63d0a4d7a8e7a6056582
mermer <mincoln419@naver.com>
2023-05-18 10:06:19 +0900

    heartbeat thread 설정 테스트

--- kafkapro01/consumers/src/main/java/com/example/ConsumerWakeUP.java
+++ kafkapro01/consumers/src/main/java/com/example/ConsumerWakeUP.java
@@ -27,7 +27,7 @@ public class ConsumerWakeUP {
         props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-01-static");
         props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.setProperty(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "2");
+        props.setProperty(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "3");
 
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props);
         kafkaConsumer.subscribe(List.of(topicName));
